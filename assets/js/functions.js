292a81a387112ee31831375d4ab3e960ad191618
marcelsafont <marcel.safont1983@gmail.com>
2022-07-04 18:25:01 +0200

    textos animats

---
+++ assets/js/functions.js
@@ -0,0 +1,23 @@
+
+
+$(window).load(function() {
+  if($(window).width() < 768){
+    $('.flexslider-mobile').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationSpeed: 600,
+      slideshowSpeed: 2000,
+    });
+  }
+  
+});
+
+$(window).bind('resize', function() { 
+
+  setTimeout(function(){ 
+      var slider = $('.flexslider-mobile').data('flexslider');   
+      slider.resize();
+  }, 1000);
+  
+  });
+
