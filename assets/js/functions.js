96d9eceec92a4c607dd6a016261ca4f3fbd21f1b
marcelsafont <marcel.safont1983@gmail.com>
2022-07-05 12:41:30 +0200

    dynamic video

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -12,54 +12,36 @@ $(window).load(function() {
   if($(window).width() > 768){
     let index = 0;
     const randomPositions = [1,2,3,4,5,6,7,8].sort( () => .5 - Math.random() );
-     randomPositions.forEach((item, index) => {
-      if(index == 0){
-        $('.video-box').append('<video class="active" src="assets/videos/video'+item+'.mp4" muted autoplay></video>');
-      }else{
-        $('.video-box').append('<video src="assets/videos/video'+item+'.mp4" muted autoplay></video>');
-      }
-      
+    $('.video-box').append('<video id="video-player" class="active" src="assets/videos/video'+randomPositions[index]+'.mp4" muted autoplay></video>');
+    $('#video-player').on('ended', function(){
+      index ++;
+      $('#video-player').attr('src', `assets/videos/video${randomPositions[index]}.mp4`)
     })
+    //  randomPositions.forEach((item, index) => {
+    //   if(index == 0){
+    //     $('.video-box').append('<video class="active" src="assets/videos/video'+item+'.mp4" muted autoplay></video>');
+    //   }else{
+    //     $('.video-box').append('<video src="assets/videos/video'+item+'.mp4" muted autoplay></video>');
+    //   }
+      
+    // })
     
-    $('.video-box video').each((index, item) => {
-      $(item).on('ended', function(){
-        $(item).removeClass('active').removeClass('hide').removeAttr('autoplay').stop();
-        if(index >= $('.video-box video').length - 1){
-          $('.video-box video').eq(0).addClass('active').get(0).play();
-        }else{
-          //$(item).next().attr('autoplaye', 'autoplay').addClass('active').get(0).play();
-          $(item).next().attr('autoplaye', 'autoplay').addClass('active')
-        }
+    // $('.video-box video').each((index, item) => {
+    //   $(item).on('ended', function(){
+    //     $(item).removeClass('active').removeClass('hide').removeAttr('autoplay').stop();
+    //     if(index >= $('.video-box video').length - 1){
+    //       $('.video-box video').eq(0).addClass('active').get(0).play();
+    //     }else{
+    //       //$(item).next().attr('autoplaye', 'autoplay').addClass('active').get(0).play();
+    //       $(item).next().attr('autoplaye', 'autoplay').addClass('active')
+    //     }
         
-      })
+    //   })
       
-      //  $(item).bind('timeupdate', updateTime(index));
+    //   //  $(item).bind('timeupdate', updateTime(index));
       
-    })
+    // })
   }
   
 });
-
-// var updateTime = function (index) { 
-//     var thisPlayer = $(this);
-//     if(this.currentTime > this.duration-1){
-
-//       thisPlayer.removeClass('active').addClass('hide');
-//       if(index >= $('.video-box video').length - 1){
-//         $('.video-box video').eq(0).addClass('active').get(0).play();
-//       }else{
-//         thisPlayer.next().addClass('active');
-//       }
-      
-//     }
-// }
-
-// $(window).bind('resize', function() { 
-
-//   setTimeout(function(){ 
-//       var slider = $('.flexslider-mobile').data('flexslider');   
-//       slider.resize();
-//   }, 1000);
-  
-//   });
 
