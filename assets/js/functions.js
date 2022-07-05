ac7b53efff5dfafd5328b569a0e849bd79b9128d
marcelsafont <marcel.safont1983@gmail.com>
2022-07-05 12:45:03 +0200

    restart loop

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,36 +11,15 @@ $(window).load(function() {
   }
   if($(window).width() > 768){
     let index = 0;
-    const randomPositions = [1,2,3,4,5,6,7,8].sort( () => .5 - Math.random() );
+    const randomPositions = [1,2,3].sort( () => .5 - Math.random() );
     $('.video-box').append('<video id="video-player" class="active" src="assets/videos/video'+randomPositions[index]+'.mp4" muted autoplay></video>');
     $('#video-player').on('ended', function(){
       index ++;
+      if(index >= randomPositions.length){
+        index = 0;
+      }
       $('#video-player').attr('src', `assets/videos/video${randomPositions[index]}.mp4`)
     })
-    //  randomPositions.forEach((item, index) => {
-    //   if(index == 0){
-    //     $('.video-box').append('<video class="active" src="assets/videos/video'+item+'.mp4" muted autoplay></video>');
-    //   }else{
-    //     $('.video-box').append('<video src="assets/videos/video'+item+'.mp4" muted autoplay></video>');
-    //   }
-      
-    // })
-    
-    // $('.video-box video').each((index, item) => {
-    //   $(item).on('ended', function(){
-    //     $(item).removeClass('active').removeClass('hide').removeAttr('autoplay').stop();
-    //     if(index >= $('.video-box video').length - 1){
-    //       $('.video-box video').eq(0).addClass('active').get(0).play();
-    //     }else{
-    //       //$(item).next().attr('autoplaye', 'autoplay').addClass('active').get(0).play();
-    //       $(item).next().attr('autoplaye', 'autoplay').addClass('active')
-    //     }
-        
-    //   })
-      
-    //   //  $(item).bind('timeupdate', updateTime(index));
-      
-    // })
   }
   
 });
