1f33aa64fd7149c59a936ad541cabe35e2046285
marcelsafont <marcel.safont1983@gmail.com>
2022-07-05 13:17:52 +0200

    animation between videos

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -12,13 +12,20 @@ $(window).load(function() {
   if($(window).width() > 768){
     let index = 0;
     const randomPositions = [1,2,3].sort( () => .5 - Math.random() );
-    $('.video-box').append('<video id="video-player" class="active" src="assets/videos/video'+randomPositions[index]+'.mp4" muted autoplay></video>');
+    $('.video-box').append('<video id="video-player" src="assets/videos/video'+randomPositions[index]+'.mp4" muted autoplay></video>');
     $('#video-player').on('ended', function(){
       index ++;
       if(index >= randomPositions.length){
         index = 0;
       }
-      $('#video-player').attr('src', `assets/videos/video${randomPositions[index]}.mp4`)
+      $('#video-player').attr('src', `assets/videos/video${randomPositions[index]}.mp4`).removeClass('loading');
+    })
+
+    $('#video-player').on('timeupdate', function(){
+      
+      if(this.currentTime > this.duration - 0.4){
+        $('#video-player').addClass('loading');
+      }
     })
   }
   
