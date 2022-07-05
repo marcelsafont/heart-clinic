e0d2f0f4b30a626899843c3d82eb22bcbc62f10c
marcelsafont <marcel.safont1983@gmail.com>
2022-07-05 14:11:05 +0200

    playsline

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   if($(window).width() > 768){
     let index = 0;
     const randomPositions = [1,2,3].sort( () => .5 - Math.random() );
-    $('.video-box').append('<video id="video-player" src="assets/videos/video'+randomPositions[index]+'.mp4" muted autoplay></video>');
+    $('.video-box').append('<video id="video-player" src="assets/videos/video'+randomPositions[index]+'.mp4" muted autoplay playsinline></video>');
     $('#video-player').on('ended', function(){
       index ++;
       if(index >= randomPositions.length){
