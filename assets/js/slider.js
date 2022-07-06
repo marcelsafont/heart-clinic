83357061783bf7a2012fb7563c9ffa8fe7dc722f
marcelsafont <marcel.safont1983@gmail.com>
2022-07-06 08:31:59 +0200

    all fixed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -6,17 +6,12 @@ constructor(container) {
   this.sliderWrap = container;
   this.sliderControl = this.sliderWrap.dataset.sliderControl || false
   this.autoplay = this.sliderWrap.dataset.sliderAutoplay || false;
-  this.timming = this.sliderWrap.dataset.sliderTimming || 500;
-  this.autoplayState = this.autoplay;
   this.slideList;
   this.slideSize;
   this.currentSlide;
-  this.controls = '.controls ul';
   this.slideElem = '.slide';
   this.activeClass = 'active';
   this.animating = false;
-  this.bullets = [];
-  this.bulletLi;
   this.init();
 }
 
@@ -25,18 +20,19 @@ init = () => {
   this.slideList = this.sliderWrap.querySelectorAll(this.slideElem);
   this.slideSize = this.slideList.length - 1;
   this.currentSlide = Math.floor(Math.random() * (this.slideSize));
+  this.currentSlide = 2;
   this.slideList = this.sliderWrap.querySelectorAll(this.slideElem);
   this.slideList[this.currentSlide].classList.add(this.activeClass);
   
   const self = this;
   this.slideList.forEach(function(el, index){
     el.querySelector('video').innerHTML = '<source  src="assets/videos/video'+(index + 1) +'.mp4" type="video/mp4" />';
-    // el.querySelector('video').addEventListener('ended', () => {
-    //   self.changeSlider(self.currentSlide);
-    //   console.log('video finito');
-    // })
+    el.querySelector('video').addEventListener('ended', () => {
+      self.changeSlider(self.currentSlide);
+    })
+    el.querySelector('video').pause();
   })
-  this.slideList[this.currentSlide].querySelector('video') && this.slideList[0].querySelector('video').play();
+  this.slideList[this.currentSlide].querySelector('video') && this.slideList[this.currentSlide].querySelector('video').play();
 }
 
 
