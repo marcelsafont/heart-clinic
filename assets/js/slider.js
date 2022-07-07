859e262c6eab306b3badaf54c496011760e53cc4
marcelsafont <marcel.safont1983@gmail.com>
2022-07-07 07:24:34 +0200

    some fixes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -17,20 +17,26 @@ constructor(container) {
 
 
 init = () => {
+  
   this.slideList = this.sliderWrap.querySelectorAll(this.slideElem);
   this.slideSize = this.slideList.length - 1;
-  this.currentSlide = Math.floor(Math.random() * (this.slideSize));
+  // random array
+  this.randomArray = [2,3,4,5,6,7,8].sort( () => .5 - Math.random() );
   this.currentSlide = 0;
-  this.slideList = this.sliderWrap.querySelectorAll(this.slideElem);
   this.slideList[this.currentSlide].classList.add(this.activeClass);
   
   const self = this;
   this.slideList.forEach(function(el, index){
-    el.querySelector('video').innerHTML = '<source  src="assets/videos/video'+(index + 1) +'.mp4" type="video/mp4" />';
+    el.querySelector('video').innerHTML = '<source  src="assets/videos/video'+ (index == 0 ? 1 : self.randomArray[0]) +'.mp4" type="video/mp4" />';
     el.querySelector('video').addEventListener('ended', () => {
       self.changeSlider(self.currentSlide);
     })
+    
     el.querySelector('video').pause();
+    if(index != 0){
+      self.randomArray.shift();
+    }
+    
   })
   this.slideList[this.currentSlide].querySelector('video') && this.slideList[this.currentSlide].querySelector('video').play();
 }
